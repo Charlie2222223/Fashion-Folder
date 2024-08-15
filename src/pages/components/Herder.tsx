619c8bd20f62d59c22e607c8d9354f682c5c50bd
Hiroto Ikegawa <koko.rara20@icloud.com>
2024-08-15 13:30:18 +0900

    レスポンシブデザインの修正

--- src/pages/components/Herder.tsx
+++ src/pages/components/Herder.tsx
@@ -1,6 +1,6 @@
 // Reactと必要なアイコンをreact-iconsライブラリからインポート
 import React from 'react';
-import { FaHome, FaUserAlt, FaCamera } from 'react-icons/fa';
+import { FaUserAlt,FaHome,FaCamera } from 'react-icons/fa';
 
 // ヘッダーコンポーネントの定義。React.FCはFunction Componentを表す型
 const Header: React.FC = () => {
@@ -8,20 +8,13 @@ const Header: React.FC = () => {
     // ヘッダー要素。Tailwind CSSを使用してスタイルを適用
     <header className="flex items-center justify-between p-4 text-white bg-gray-800">
       
-      {/* ロゴセクション：アイコンとアプリ名を表示 */}
+      {/* ロゴセクション：アプリ名を表示 */}
       <div className="flex items-center">
-        <FaHome size={30} /> {/* Homeアイコンの表示 */}
-        <h1 className="ml-2 text-xl">My Fashion App</h1> {/* アプリ名の表示 */}
+        <h1 className="ml-2 text-xl">Fashion Folder</h1> {/* アプリ名の表示 */}
       </div>
 
       {/* ナビゲーションメニュー */}
       <nav className="flex">
-        
-        {/* Homeリンク：アイコンとテキストを表示 */}
-        <a href="#home" className="flex items-center ml-4 text-white hover:text-gray-400">
-          <FaHome size={20} /> 
-          <span className="ml-2">Home</span> 
-        </a>
         
         {/* Profileリンク：アイコンとテキストを表示 */}
         <a href="#profile" className="flex items-center ml-4 text-white hover:text-gray-400">
@@ -29,11 +22,18 @@ const Header: React.FC = () => {
           <span className="ml-2">ユーザー</span>
         </a>
         
-        {/* Cartリンク：アイコンとテキストを表示 */}
-        <a href="#cart" className="flex items-center ml-4 text-white hover:text-gray-400">
-          <FaCamera size={20} />
-          <span className="ml-2">写真</span> 
-        </a>
+        {/* 他の要素は小さいスクリーンで隠す */}
+        <div className="hidden lg:flex">
+          {/* 他のリンクをここに追加 */}
+          <a href="#home" className="flex items-center ml-4 text-white hover:text-gray-400">
+            <FaHome size={20} /> 
+            <span className="ml-2">Home</span> 
+          </a>
+          <a href="#cart" className="flex items-center ml-4 text-white hover:text-gray-400">
+            <FaCamera size={20} />
+            <span className="ml-2">写真</span> 
+          </a>
+        </div>
         
       </nav>
     </header>
