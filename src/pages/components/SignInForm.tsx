7809380537d7902a7d3beb68b99e703247cc5087
Hiroto Ikegawa <koko.rara20@icloud.com>
2024-08-15 16:07:18 +0900

    細かいレイアウトの修正

--- src/pages/components/SignInForm.tsx
+++ src/pages/components/SignInForm.tsx
@@ -12,29 +12,29 @@ const SignInForm: React.FC<{ onClose: () => void }> = ({ onClose }) => {
           ✖
         </button>
         <h1 className="text-2xl font-bold text-gray-800 dark:text-white text-center mb-4">
-          Sign in
+          サインイン
         </h1>
         <p className="text-sm text-gray-600 dark:text-neutral-400 text-center mb-6">
-          Don&apos;t have an account yet?{" "}
+          まだアカウントをお持ちでないですか？{" "}
           <a className="text-blue-600 decoration-2 hover:underline dark:text-blue-500" href="#">
-            Sign up here
+            こちらで登録
           </a>
         </p>
         <button className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
-        <FcGoogle className="mr-2 text-2xl" />
-          Sign in with Google
+          <FcGoogle className="mr-2 text-2xl" />
+          Googleでサインイン
         </button>
         <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">
-          Or
+          または
         </div>
         <form>
           <div className="grid gap-y-4">
             <div>
               <label className="block text-sm mb-2 dark:text-white" htmlFor="email">
-                Email address
+                メールアドレス
               </label>
               <input
-                className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                className="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:placeholder-neutral-500 dark:focus:bg-neutral-800 dark:focus:ring-neutral-600 shadow-sm"
                 id="email"
                 name="email"
                 required
@@ -44,31 +44,31 @@ const SignInForm: React.FC<{ onClose: () => void }> = ({ onClose }) => {
             <div>
               <div className="flex justify-between items-center">
                 <label className="block text-sm mb-2 dark:text-white" htmlFor="password">
-                  Password
+                  パスワード
                 </label>
-                <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline dark:text-blue-500" href="#">
-                  Forgot password?
-                </a>
               </div>
               <input
-                className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                className="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:placeholder-neutral-500 dark:focus:bg-neutral-800 dark:focus:ring-neutral-600 shadow-sm"
                 id="password"
                 name="password"
                 required
                 type="password"
               />
+              <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline dark:text-blue-500" href="#">
+                  パスワードをお忘れですか？
+                </a>
             </div>
             <div className="flex items-center">
               <input className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="remember-me" name="remember-me" type="checkbox" />
               <label className="ms-3 text-sm dark:text-white" htmlFor="remember-me">
-                Remember me
+                ログイン状態を維持する
               </label>
             </div>
             <button className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700">
-              Sign in
+              サインイン
             </button>
           </div>
-        </form>
+        </form> 
       </div>
     </div>
   );
