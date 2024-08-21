1c3e9c402166e9e6ddb8fd747c7881b314391c56
Hiroto Ikegawa <koko.rara20@icloud.com>
2024-08-22 01:17:47 +0900

    アイコンの追加

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,26 +1,45 @@
-import { FaCamera, FaHome, FaUserAlt } from "react-icons/fa";
+import { useEffect } from "react";
+import { useRouter } from "next/router"; 
+import { FaHome, FaUserAlt } from "react-icons/fa";
 
 const Header: React.FC<{ onUserClick: () => void }> = ({ onUserClick }) => {
+  const router = useRouter(); 
+
+  useEffect(() => {
+    console.log("Header component mounted");
+  }, []);
+
+  const IconClick = () => {
+    console.log("Icon clicked");
+    router.push('/SignIn'); 
+  };
 
   return (
-    <header className="flex items-center justify-between p-4 text-white bg-gray-800">
-      <div className="flex items-center">
-        <h1 className="ml-2 text-xl">Fashion Folder</h1>
+    <header className="flex items-center justify-between p-3 text-white bg-gray-800">
+      <div className="flex items-center" onClick={IconClick}>
+        <img 
+          className="object-cover w-20 h-20 opacity-75 cursor-pointer"  
+          src="/img/Icon2.png" 
+          alt="Gallery Masonry Image" 
+        />
+        <h1 className="ml-4 text-4xl cursor-pointer">
+          Fashion Folder
+        </h1>
       </div>
       <nav className="flex">
         <button 
           onClick={() => {
-            onUserClick();  // ここでフォームを表示するための関数を呼び出し
+            onUserClick();
           }}
-          className="flex items-center ml-4 text-white hover:text-gray-400 z-20"
+          className="z-20 flex items-center ml-6 text-white hover:text-gray-400"
         >
-          <FaUserAlt size={20} />
-          <span className="ml-2">ユーザー</span>
+          <FaUserAlt size={28} />
+          <span className="ml-3 text-lg">ユーザー</span> 
         </button>
         <div className="hidden lg:flex">
-        <a href="#home" className="flex items-center ml-4 text-white hover:text-gray-400">
-          <FaHome size={20} /> 
-          <span className="ml-2">Home</span> 
+        <a href="#home" className="flex items-center ml-6 text-white hover:text-gray-400">
+          <FaHome size={28} />
+          <span className="ml-3 text-lg">Home</span>
         </a>
       </div>
       </nav>
