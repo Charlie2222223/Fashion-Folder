b6a8d978aee820d8a557233caa0764445961463d
Hiroto Ikegawa <koko.rara20@icloud.com>
2024-09-24 22:58:57 +0900

    ダッシュボードをリロードしたら画面が見つからなくなるバグの修正

---
+++ src/pages/user-modal/PasswordInput.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Background from '../components/Background'
+import PasswordInputForm from '../components/user/login/setting/password/Input/PasswordInputForm'
+
+const PasswordInput: React.FC = () => {
+  return (
+    <main>
+      <Background />
+      <PasswordInputForm />
+    </main>
+  )
+}
+
+export default PasswordInput
