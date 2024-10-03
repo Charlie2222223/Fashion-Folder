77fd947220a2d64d9e243476e149faa55c40b63c
Hiroto Ikegawa <koko.rara20@icloud.com>
2024-10-03 12:30:18 +0900

    setupテーブルのモデルを作成

---
+++ Server/app/Models/clothes_setup_items.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class clothes_setup_items extends Model
+{
+    protected $table = 'clothes_setup_items';
+
+    use HasFactory;
+}
