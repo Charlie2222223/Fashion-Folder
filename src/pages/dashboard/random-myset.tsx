7c03da400e99c2c689973a6a5b67f902accf32b0
Charlie <158181191+Charlie2222223@users.noreply.github.com>
2024-10-05 01:16:04 +0900

    いらないコードを削除

--- src/pages/dashboard/random-myset.tsx
+++ src/pages/dashboard/random-myset.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from '../components/closet/Layout';
-import ClothingRegistration from '../components/closet/randomsetup/RandomSetup';
 import RandomSetup from '../components/closet/randomsetup/RandomSetup';
 
 const RegisterClothesPage: React.FC = () => {
