25358bfc3cbd3b4dac23b30b08d5bd95a89c0a46
Charlie <158181191+Charlie2222223@users.noreply.github.com>
2024-10-09 01:05:21 +0900

    エラーの修正

---
+++ Server/storage/framework/views/e693ee68698c35a588a46379f147000957b305b0.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>  
+<h1>仮メールのご案内</h1>
+<p>あなたのパスワードは<?php echo e($temporary_password); ?>です</p>
+</body>
+</html><?php /**PATH /var/www/resources/views/emails/welcome.blade.php ENDPATH**/ ?>
