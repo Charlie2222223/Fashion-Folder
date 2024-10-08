e322ec14ba92b9ff7246e9d820fd48a753334c84
Charlie <158181191+Charlie2222223@users.noreply.github.com>
2024-10-09 01:18:19 +0900

    SignInできない問題を解決

--- src/pages/components/user_auth/signin/SigninForm.tsx
+++ src/pages/components/user_auth/signin/SigninForm.tsx
@@ -29,7 +29,7 @@ const SigninForm: React.FC = () => {
   
       // 正常に登録された場合、次のページにPOSTリクエストでデータを渡す
       router.push({
-        pathname: '/Verifications',
+        pathname: 'auth/Verifications',
         query: {
           name: formData.name,
           email: formData.email,
