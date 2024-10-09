28d99a9dfc5cde7f731b2fbc36d9cc9d59588d32
Charlie <158181191+Charlie2222223@users.noreply.github.com>
2024-10-09 10:00:15 +0900

    フィルタリングのボタンの位置を右に移動

--- src/pages/components/closet/clotheslist/FilterButton.tsx
+++ src/pages/components/closet/clotheslist/FilterButton.tsx
@@ -9,7 +9,7 @@ interface FilterButtonProps {
 
 const FilterButton: React.FC<FilterButtonProps> = ({ viewMode, openFilterModal }) => {
   return (
-    <div className="mb-4">
+    <div className="flex justify-end mb-4"> {/* ボタンを右寄せ */}
       {viewMode === 'clothes' && (
         <button
           className="px-4 py-2 mb-4 text-white bg-indigo-600 rounded-md"
